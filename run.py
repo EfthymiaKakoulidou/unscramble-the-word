633d4c6f83ec99d8a29a824b8387bbd74daaeb50
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-01-13 22:50:23 +0000

    Add nested lists to shuffle the letters of the words

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
-animals = ['cat','dog','horse']
-countries = ['greece', 'italy', 'sweden']
-food = ['pasta', 'rice', 'meat']
+import random
+
+animals = [['c','a','t'],['d','o','g'],['h','o','r','s','e']]
+countries = [['g','r','e','e','c','e'],['i','t','a','l','y'], ['s','w','e','d','e','n']]
+food = [['p','a','s','t','a'], ['r','i','c','e'], ['m','e','a','t']]
 
 print('Hello Tim! Ready to unscrable the word?')
 print('You will be given words whose letters are scrambled.')
 print('Your goal is to unscramble them and find the word')
-print('Please provide the number of the category of words you want to play with')
+print('Please provide the number of the category of words you want to play with:')
 print('1.Animals 2. Countries 3. Food')
 
-def category_selection():
+x = input('Write the number of the category here:')
+print('You Chose ' + x)
+word = random.choice(animals)
+print(word)
+random.shuffle(word)
+print(word)
 
