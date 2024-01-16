b8fa1863d460046aed6daee55b55b928e0bec172
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-01-16 18:03:12 +0000

    Add comments to functions

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from colorama import Fore
 score = 0
 
 def game(score):
+
     # Categories
     
     animals = ['cat','dog','horse']
@@ -20,34 +21,43 @@ def game(score):
     print('Your goal is to unscramble them and find the word.\n')
     print('Please provide the number of the category of words you want to play with:\n')
     print('1. Animals 2. Countries 3. Food \n')
-    number = input("Enter your choice here:")
-
+    
     # Player chooses category
 
     def category_choice():
+        """
+        Asks for input to choose the category.
+        Validates it.
+        """
         while True:
-            
+            number = input("Enter your choice here:")
             if number == '1':
                 print('You chose "Animals"\n')
                 scramble_word(animals)
-                break
                 
             elif number == '2':
                 print('You chose "Countries"\n')
                 scramble_word(countries)
-                break
                 
             elif number == '3':
                 print('You chose "Food"\n')
                 scramble_word(food)
-                break
+
             else:
                 print(f'{number} is invalid. Please try again.')
+                category_choice()
+            return True
 
     # Function that scrambles the word from the category the player chose
 
     def scramble_word(category):
-        
+        """
+        Chooses randomly an item from the list chosen.
+        Takes it out of the list.
+        Shyffles the item/word.
+        Calls the player's answer function.
+        Calls the play again function when there are no items left on the list.
+        """
         while len(category) > 0 :
             print('Your scrambled word is:\n')
             unscrambled_word = random.choice(category)
@@ -61,15 +71,18 @@ def game(score):
             print(Fore.RED + scrambled_word  + Fore.RESET)
             players_answer(unscrambled_word, scrambled_word)
         else:
-            print("Game Over")
+            print("No words left to unscramble. Game Over")
             play_again()
         
-    def clear():
-        os.system('cls' if os.name == 'nt' else 'clear')
+    #def clear():
+        #os.system('cls' if os.name == 'nt' else 'clear')
 
     # Players answer
 
     def players_answer(unscrambled_word, scrambled_word):
+        """
+        Player's input and validation
+        """
         while True:
             players_guess = input('Unscramble here:\n').lower()
             if validation_words(players_guess, unscrambled_word,scrambled_word):
@@ -79,13 +92,13 @@ def game(score):
 
     def validation_words(players_guess, unscrambled_word,scrambled_word):
         """
-        Raises error if the input is not of the correct type, in the correct order
-        or does not have the correct length
+        Raises error if the input is not of correct
+        Gives the option to retry
         """
         global score
         if str(players_guess) == str(unscrambled_word):
             score = score + 1
-            print(f'Right! Your Score is : {score} ')
+            print(f'Right! Your Score is : {score}')
             return True
 
         else:
@@ -93,7 +106,7 @@ def game(score):
             retry = input('Do you want to try again? y/n : ')
             if retry == 'n':
                 print('Next word:')
-                category_choice()
+               
             elif retry == 'y':
                 players_answer(unscrambled_word, scrambled_word)
                 return True
@@ -107,13 +120,12 @@ def game(score):
     def play_again():
         while True:
             answer = input('Would you like to play again y/n?\n').lower()
-
             clear()
             if answer == 'n':
                 print('Thanks for playing')
                 exit()
             elif answer == 'y':
-                game(reset_score)
+                game(score)
             else:
                 print(f'{answer} is invalid. Please try again.')
                 break
@@ -121,6 +133,6 @@ def game(score):
     # Calls the functions and starts the game
 
     if __name__ == '__main__':
-        clear()
+       # clear()
         category_choice()
 game(score)
