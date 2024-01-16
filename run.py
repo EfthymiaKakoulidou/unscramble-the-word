537857c0ba051e0fd244dd9a9af1bff4b0ccf98c
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-01-16 06:04:41 +0000

    Add the function to play again, clear and refactor

--- run.py
+++ run.py
@@ -1,94 +1,76 @@
 import random
-from random import shuffle
+import os
 
 animals = ['cat','dog','horse']
 countries = ['greece','italy', 'sweden']
 food = ['pasta', 'rice', 'eggs']
 
-print('Hello Tim! Ready to unscrable the word?\n')
-print('You will be given words whose letters are scrambled.\n')
-print('Your goal is to unscramble them and find the word.\n')
-print('Please provide the number of the category of words you want to play with:\n')
-print('1.Animals 2. Countries 3. Food\n')
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-number = input("Enter your choice here:")
+def players_answer(unscrambled_word, scrambled_word):
+    while True:
+        players_guess = input('Unscramble here:\n').lower()
+        clear()
+        print(scrambled_word)
+        if validation_words(players_guess, unscrambled_word):
+            break
 
-def category_choice(number):
+def play_again():
+    while True:
+        answer = input('Would you like to play again Y/N?\n').lower()
+
+        clear()
+        if answer == 'n':
+            print('Thanks for playing')
+            exit()
+        elif answer == 'y':
+            category_choice()
+        else:
+            print(f'{answer} is invalid. Please try again.')
+            break
+            
+def scramble_word(category):
+    
+    while len(category) > 0 :
+        print('Your scrambled word is:\n')
+        unscrambled_word = random.choice(category)
+        x = category.index(unscrambled_word)
+        category.pop(x)
+        l = list(unscrambled_word)
+        random.shuffle(l)
+        while l == unscrambled_word:
+            random.shuffle(l)
+        scrambled_word = ''.join(l)
+        print(scrambled_word)
+        players_answer(unscrambled_word, scrambled_word)
+    else:
+        print("Game Over")
+        play_again()
+
+def category_choice():
+    while True:
+        print('1. Animals 2. Countries 3. Food \n')
+        number = input("Enter your choice here:")
+        clear()
+
         if number == '1':
             print('You chose "Animals"\n')
-            while len(animals) > 0 :
-                print('Your scrambled word is:\n')
-                unscrambled_word = random.choice(animals)
-                x = animals.index(unscrambled_word)
-                animals.pop(x)
-                l = list(unscrambled_word)
-                shuffle(l)
-                scrambled_word = ''.join(l)
-                print(scrambled_word)
-                def players_answer():
-                    while True:
-                        players_guess = input('Unscramble here:\n')
-                        if validation_words(players_guess, unscrambled_word):
-                            break
-                players_answer()
-            else:
-                print("Game Over")
-               
-        if number == '2':
+            scramble_word(animals)
+            break
+            
+        elif number == '2':
             print('You chose "Countries"\n')
-            while len(countries) > 0 :
-                print('Your scrambled word is:\n')
-                unscrambled_word = random.choice(countries)
-                x = countries.index(unscrambled_word)
-                countries.pop(x)
-                l = list(unscrambled_word)
-                shuffle(l)
-                scrambled_word = ''.join(l)
-                print(scrambled_word)
-                def players_answer():
-                    while True:
-                        players_guess = input('Unscramble here:\n')
-                        if validation_words(players_guess, unscrambled_word):
-                            break
-                players_answer()
-            else:
-                print("Game Over")
-                
-        if number == '3':
+            scramble_word(countries)
+            break
+            
+        elif number == '3':
             print('You chose "Food"\n')
-            while len(food) > 0 :
-                print('Your scrambled word is:\n')
-                unscrambled_word = random.choice(food)
-                x = food.index(unscrambled_word)
-                food.pop(x)
-                l = list(unscrambled_word)
-                shuffle(l)
-                scrambled_word = ''.join(l)
-                print(scrambled_word)
-                def players_answer():
-                    while True:
-                        players_guess = input('Unscramble here:\n')
-                        if validation_words(players_guess, unscrambled_word):
-                            break
-                players_answer()
-                break
-            else:
-                print("Game Over")
-
-def validation_numbers(number):
-    """
-    Raises error if the input is not 1,2 or 3
-    """
-    try:
-        if number != '1' or '2' or '3':
-            print('You have to choose one of the numbers 1,2 or 3.Please try again.\n')
-            return False
-        return True
-    except:
-        print('You have to choose one of the numbers 1,2 or 3.Please try again.\n')
-        return False
-    return True
-
+            scramble_word(food)
+            break
+        else:
+            print(f'{number} is invalid. Please try again.')
+    
 
 def validation_words(players_guess, unscrambled_word):
 
@@ -97,24 +79,21 @@ def validation_words(players_guess, unscrambled_word):
     or does not have the correct length
     """
 
-    if str(players_guess)== str(unscrambled_word):
-        print('Right! Next word:')
-        category_choice(number)
+    if str(players_guess) == str(unscrambled_word):
+        print('Right!')
+        return True
+       # category_choice(number)
 
-    try:
-        if str(players_guess)!= str(unscrambled_word):
-            raise ValueError(
-                'The answer you provided is wrong'
-            )
-    except ValueError as e:
-        print('Please try again.\n')
+    else:
+        print('The answer you provided is wrong')
         return False
-    return True
 
-def main():
-    """
-    Calls all the functions
-    """
-    category_choice(number)
-
-main()
+if __name__ == '__main__':
+    clear()
+    print('Hello Tim! Ready to unscrable the word?\n')
+    print('You will be given words whose letters are scrambled.\n')
+    print('Your goal is to unscramble them and find the word.\n')
+    print('Please provide the number of the category of words you want to play with:\n')
+    
+    category_choice()
+  
